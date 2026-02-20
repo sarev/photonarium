cad1b527932c53823b7280207deb05324ee53b50
Steve Revill <steve@7thsoftware.co.uk>
2026-02-20 22:49:59 +0000

    Website updates

---
+++ tools/www-update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+GENERATED=./generated
+IMAGES=./www/images
+TUTORIAL=./www/tutorial
+
+if [ -d ${GENERATED}/screenshots ]; then
+    cp -fv ${GENERATED}/screenshots/*.png ${TUTORIAL}/screenshots/
+fi
+if [ -f ${GENERATED}/index.html ]; then
+    cp -fv ${GENERATED}/index.html ${TUTORIAL}/
+fi
+if [ -d ${IMAGES} ]; then
+    cp -fv ${TUTORIAL}/screenshots/4-4.png  ${IMAGES}/duplicates.png
+    cp -fv ${TUTORIAL}/screenshots/6-14.png ${IMAGES}/faces.png
+    cp -fv ${TUTORIAL}/screenshots/2-1.png  ${IMAGES}/fullscreen.png
+    cp -fv ${TUTORIAL}/screenshots/2-5.png  ${IMAGES}/gallery.png
+    cp -fv ${TUTORIAL}/screenshots/1-4.png  ${IMAGES}/info-panel.png
+    cp -fv ${TUTORIAL}/screenshots/8-2.png  ${IMAGES}/light-theme.png
+fi
