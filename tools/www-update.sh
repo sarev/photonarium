5014d8fa17dbfd2dabf1db2d0815a1edd262f190
Steve Revill <steve@7thsoftware.co.uk>
2026-03-08 12:09:23 +0000

    Website updates

--- tools/www-update.sh
+++ tools/www-update.sh
@@ -16,5 +16,6 @@ if [ -d ${IMAGES} ]; then
     cp -fv ${TUTORIAL}/screenshots/2-1.png  ${IMAGES}/fullscreen.png
     cp -fv ${TUTORIAL}/screenshots/2-5.png  ${IMAGES}/gallery.png
     cp -fv ${TUTORIAL}/screenshots/1-4.png  ${IMAGES}/info-panel.png
-    cp -fv ${TUTORIAL}/screenshots/8-2.png  ${IMAGES}/light-theme.png
+    cp -fv ${TUTORIAL}/screenshots/8-8.png  ${IMAGES}/heatmap.png
+    cp -fv ${TUTORIAL}/screenshots/9-2.png  ${IMAGES}/light-theme.png
 fi
